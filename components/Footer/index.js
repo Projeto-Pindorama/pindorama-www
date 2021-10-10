cc192cdf2c49725982dc0472e9f0429708d86b02
Vitor <vi.souza.almeida@protonmail.com>
2021-10-10 15:45:32 -0300

    style: adição dos gifs/imagens no rodapé

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,17 +1,22 @@
+import Image from 'next/image'
+import Linux from '../../img/linux.gif'
+import CopaCabana from '../../img/COPACABANA_X64-256.102x46.png'
+import Hk from '../../img/WESTANDWITHHK.png'
+
 export default () => {
     return (
         <footer>
             <div className="text-center">
-                <div className="container">
+                <div className="container p-2">
                     <div className="row">
                         <div className="col-sm">
-                            Gif 1
+                        <Image src={Linux}></Image>
                         </div>
                         <div className="col-sm">
-                        Gif 1
+                        <Image src={Hk}></Image>
                         </div>
                         <div className="col-sm">
-                        Gif 1
+                        <Image src={CopaCabana}></Image>
                         </div>
                     </div>
                 </div>
